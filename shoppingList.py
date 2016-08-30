678bc614f2ae6ee1b9c50cb45d3a24482bde7640
nishiluke <nishiluke@gmail.com>
2016-08-30 22:29:42 +1000

    Finished mainMenu function

--- shoppingList.py
+++ shoppingList.py
@@ -1,31 +1,40 @@
-import os
-
 MENU_TEXT = ["R", "List required items", "C", "List completed items", "A", "Add new item",
              "M", "Mark an item as completed", "Q", "Quit"]
 
 
 def main():
-    itemList = open("items.csv", "w+")
+    itemList = open("items.csv", "r+")
     print("Shopping List 1.0 - by Luke West")
     rowCount = 0
     for row in itemList:
-        rowCount += row
+        rowCount += 1
     print("{} items loaded from items.csv".format(rowCount))
-    menuInput = mainMenu()
 
+    menuInput = ""
+    while menuInput != "Q":
+        menuInput = mainMenu()
+        if menuInput == "R":
+            print("Required items:")
+        elif menuInput == "C":
+            print("C")
+        elif menuInput == "A":
+            print("A")
+        elif menuInput == "M":
+            print("M")
+        elif menuInput == "Q":
+            print("Thanks for using the shopping list")
 
 def mainMenu():
-    output = ""
-    while output not in MENU_TEXT[::2]:
+    userInput = ""
+    while userInput not in MENU_TEXT[::2]:
         print("Menu:")
         for item in range(5):
             item *= 2
             print("{} - {}".format(MENU_TEXT[item], MENU_TEXT[item+1]))
-        output = str(input(">>>")).upper
-        if output not in MENU_TEXT[::2]:
+        userInput = str(input(">>>")).upper()
+        if userInput not in MENU_TEXT[::2]:
             print("Invalid menu choice")
-            print(MENU_TEXT[::2])
-    return output.lower
+    return userInput
 
 
 main()
